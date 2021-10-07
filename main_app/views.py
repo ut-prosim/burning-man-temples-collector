ea6e828df96e34034fcf4fcd04abb30697c7ca01
brian kasten <panphobicdream@gmail.com>
2021-10-06 23:26:05 -0400

    initial setup

---
+++ main_app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse
+from django.views.generic.base import TemplateView
+
+class Home(TemplateView):
+    template_name = 'home.html'
+
+class About(TemplateView):
+    template_name = 'about.html'
+
